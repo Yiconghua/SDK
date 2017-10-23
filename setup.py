2e8fcedb66a43e59e159f6dcfb7790cb2a26ec8d
杨功廷 <gongting.yang@ele.me>
2017-10-23 16:33:56 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eleme.openapi.python.sdk',
-    version='1.8.0',
+    version='1.9.0',
     keywords=('eleme', 'openapi'),
     #long_description=open('README.md').read(),
     description='eleme openapi python sdk',
